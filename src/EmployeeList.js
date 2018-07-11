72091a8bbb648da5cece7a9e252fb2102851387b
sdana <seth.dana@gmail.com>
2018-07-11 15:16:47 -0500

    Realized I did exercise wrong, going to refactor

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 
 export default class EmployeeList extends Component {
   render() {
-    console.log(this.props.employees);
+    console.log(this.props.employees)
     return (
       <React.Fragment>
-        {
-            this.props.employees.map(employee =>
-                <div>
-                    <h3>{employee.name}</h3>
-                </div>
-        )
-        }
+        {this.props.employees.map(employee => (
+          <div className="employee-div">
+            <h3>{employee.name}</h3>
+          </div>
+        ))}
       </React.Fragment>
-    );
+    )
   }
 }

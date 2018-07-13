a0a4cf2226ec0fabf97e49752bb6b904eeccab08
sdana <seth.dana@gmail.com>
2018-07-13 11:01:50 -0500

    styled nav bar, added login page with local/session storage

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -6,18 +6,29 @@ import LocationList from "./LocationList"
 import Location from "./Location"
 import EmployeeList from "./EmployeeList"
 import Employee from "./Employee"
+import Login from "./Login"
 
 export default class ApplicationViews extends Component {
+  isAuthenticated = () => localStorage.getItem("credentials") !== null
+
   render() {
-    return (
-      <React.Fragment>
-        <Route exact path="/" component={LocationList} />
-        <Route path="/locations/:locationId" render={(props)=>{return <Location locations={props.location.state.locations} />}} />
-        <Route exact path="/animals" component={AnimalList} />
-        <Route path="/animals/:animalId" render={(props) => {return <Animal animal={props.location.state.animal} />}} />
-        <Route exact path="/employees" component={EmployeeList} />
-        <Route path="/employees/:employeeId" render={(props) => {return <Employee employee={props.location.state.employee} />}} />
-      </React.Fragment>
-    )
+    if (this.isAuthenticated()){
+      return (
+        <React.Fragment>
+          <Route path="/login" component={Login} />
+          <Route exact path="/" component={LocationList} />
+          <Route path="/locations/:locationId" render={(props)=>{return <Location locations={props.location.state.locations} />}}/>
+          <Route exact path="/animals" component={AnimalList} />
+          <Route path="/animals/:animalId" render={(props) => {return <Animal animal={props.location.state.animal} />}} />
+          <Route exact path="/employees" component={EmployeeList} />
+          <Route path="/employees/:employeeId" render={(props) => {return <Employee employee={props.location.state.employee} />}} />
+        </React.Fragment>
+      )
+    }
+    else {
+      return (
+        <Route path="/" component={Login} />
+      )
+    }
   }
 }
--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -4,12 +4,14 @@ import "./NavBar.css"
 
 export default class NavBar extends Component {
   render() {
-    return (
-      <nav>
-        <Link to="/">Locations</Link>
-        <Link to="/animals">Animals</Link>
-        <Link to="/employees">Employees</Link>
+    return <nav>
+        <Link to="/">
+          Locations
+        </Link>
+        <Link to="/animals">
+          Animals
+        </Link>
+      <Link to="/employees">Employees</Link>
       </nav>
-    )
   }
 }
